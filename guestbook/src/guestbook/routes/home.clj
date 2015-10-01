e8ff0e36d043041688cd5a88389159b7762d42b5
sleyzerzon <simeon@zipari.com>
2015-10-01 17:00:23 -0400

    added form

--- guestbook/src/guestbook/routes/home.clj
+++ guestbook/src/guestbook/routes/home.clj
@@ -1,18 +1,39 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [guestbook.views.layout :as layout]))
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]))
 
-(defn home []
+(defn home [& [name message error]]
   (layout/common 
-    [:h1 "Guestbook"]
+
+    [:h1 "Guestbook"] 
     [:p "Welcome to my guestbook"]
-    [:hr]
-    [:form 
-     [:p "Name:"]
-     [:input]
-     [:p "Message"]
-     [:textarea {:rows 10 :cols 40}]
-     ]))
+    [:p error]
+    ;;here we call our show-guests function
+;    to generate the list of existing comments
+(show-guests)
+[:hr]
+;here we create a form with text fields called "name" and "message"
+;these will be sent when the form posts to the server as keywords of the same name
+(form-to [:post "/"]
+         [:p "Name:"]
+         (text-field "name" name)
+         [:p "Message: "]
+         (text-area {:rows 10 :cols 40} "message" message)
+         [:br]
+         (submit-button "comment")) 
+    ))
+
+
+(defn show-guests []
+  [:ul.guests 
+   (for [{:keys [message name timestamp]}
+          [{:message "Howdy" :name "Bob" :timestamp nil}
+         {:message "Howdy" :name "Bob" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+       [:time timestamp]])]) 
 
 (defroutes home-routes
   (GET "/" [] (home)))
