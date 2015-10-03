b4466223daeaab0176e038dfc7ffc70cd1d1449e
sleyzerzon <senya22@yahoo.com>
2015-10-03 18:31:10 -0400

    clean

--- clj-maven-tutorial/src/main/clojure/chaomancy/maven.clj
+++ clj-maven-tutorial/src/main/clojure/chaomancy/maven.clj
@@ -1,13 +1,6 @@
 (ns chaomancy.maven
-
   (:gen-class))
-
- 
-
 (defn exclaim []
-
       "Yes, I wrote Clojure in Eclipse and tested/ran/deployed it with Maven!")
-
- 
 
 (defn -main [& args] (println (exclaim)))
